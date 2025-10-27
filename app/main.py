36e7dac9d377a23fb1233d30b9f15144e5c8b8bd
Jeffrey Omoakah <jomoakah3@gmail.com>
2025-10-27 09:12:47 +0100

    feat: add main app script

---
+++ app/main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+from .routers import users, auth, posts
+from .database import engine
+from . import models
+
+models.Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+
+app.include_router(auth.router)
+app.include_router(users.router)
+app.include_router(posts.router)
