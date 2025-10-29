f001f9972a1db66cf448acb02fcce188a36ddbe6
Jeffrey Omoakah <jomoakah3@gmail.com>
2025-10-29 15:42:20 +0100

    feat: Handles JWT creation and validation.

---
+++ app/oauth2.py
@@ -0,0 +1,19 @@
+from jose import JWTError, jwt
+from datetime import datetime, timedelta
+from . import schemas, database, models
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from sqlalchemy.orm import Session
+from .config import settings
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
+
+SECRET_KEY = settings.secret_key
+ALGORITHM = settings.algorithm
+ACCESS_TOKEN_EXPIRE_MINUTES = settings.access_token_expire_minutes
+
+def create_access_token(data: dict):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
