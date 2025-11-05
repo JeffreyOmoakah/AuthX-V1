ee9682bce6b0e935af3c6d4e1eb4b752887f8005
Jeffrey Omoakah <jomoakah3@gmail.com>
2025-11-05 19:44:39 +0100

    feat: Defined how data moves between frontend ↔ backend using Pydantic models.

---
+++ app/schemas.py
@@ -0,0 +1,15 @@
+from pydantic import BaseModel, EmailStr
+
+class UserCreate(BaseModel):
+    email: EmailStr
+    password: str
+
+class UserOut(BaseModel):
+    id: int
+    email: EmailStr
+    class Config:
+        orm_mode = True
+
+class PostCreate(BaseModel):
+    title: str
+    content: str
